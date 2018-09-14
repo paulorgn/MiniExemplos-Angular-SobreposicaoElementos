9a3dd3d328aae761e9db47fc8635bab177a2b40e
paulorgn <paulorgndev@gmail.com>
2018-09-14 00:40:52 -0300

    Sobrepondo componentes

--- Sobreposicao/src/app/app.module.ts
+++ Sobreposicao/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CabecalhoComponent } from './cabecalho/cabecalho.component';
+import { CorpoComponent } from './corpo/corpo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabecalhoComponent,
+    CorpoComponent
   ],
   imports: [
     BrowserModule
